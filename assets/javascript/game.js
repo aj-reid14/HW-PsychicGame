dcf217b51a3bb4890788ba5b82033419ca1eb4e2
ajreid <axr490@miami.edu>
2019-10-03 08:12:20 -0400

    added logic to check for already guessed keys

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 let wins = 0;
 let losses = 0;
 let guessesLeft = 10;
+let alreadyGuessed = [];
 let gameActive = true;
 let winSound = new Audio("assets/audio/win.wav");
 let lossSound = new Audio("assets/audio/loss.wav");
@@ -19,10 +20,14 @@ document.onkeyup = function (event)
     if (gameActive) 
     {
         let keyGuessed = event.key;
-        console.log(keyGuessed + ": " + event.keyCode + ", Correct: " + randomKey);
+        alreadyGuessed.push(keyGuessed);
+
+        // Un-Comment the following code to see 'alreadyGuessed' update & correct letter in console
+        console.log("User Guessed: " + keyGuessed + ", Correct: " + randomKey);
+        console.log(alreadyGuessed);
 
         // Game Logic: Check User's Guess against AI's Selection
-        if (randomKey === event.key) 
+        if (randomKey === keyGuessed) 
         {
             gameActive = false; // The game has ended, user has won
             winSound.play();
@@ -34,9 +39,18 @@ document.onkeyup = function (event)
         {
             if (guessesLeft > 0) 
             {
-                guessesLeft--;
-                document.getElementById("p_guessed").append(" " + keyGuessed);
-                document.getElementById("p_guess").textContent = guessesLeft;
+                
+                if (!keyAlreadyGuessed(keyGuessed)) 
+                {
+                    document.getElementById("p_guessed").append(" " + keyGuessed);
+                    document.getElementById("p_guess").textContent = guessesLeft;
+                    document.getElementById("game_msg").textContent = "";
+                    guessesLeft--;
+                }
+                else
+                {
+                    document.getElementById("game_msg").textContent = "'" + keyGuessed + "' already guessed!";
+                }
             }
             else 
             {
@@ -58,6 +72,33 @@ function restartGame()
         document.getElementById("p_guessed").textContent = "";
         document.getElementById("game_msg").textContent = "";
         randomKey = String.fromCharCode((Math.floor(Math.random() * 26) + 65)).toLowerCase(); // New Random Letter From Computer
+        alreadyGuessed = [];
         gameActive = true;
     }
+}
+
+function keyAlreadyGuessed(key)
+{
+    // Boolean that will update (only) if key has already been guessed
+    let keyExists = false;
+
+    if (alreadyGuessed.length > 0)
+    {
+        // Since 'keyGuessed' is added to the array before this function is called,
+        // Loop through 'arr.length - 1', since the newest entry will always equal 'key' 
+        for (let i = 0; i < alreadyGuessed.length - 1; i++)
+        {
+            // If the currently guessed key is already present in the array
+            if (alreadyGuessed[i] === key)
+            {
+                keyExists = true;
+
+                // Remove the newest entry from the array, which is the currently guessed 'key'
+                alreadyGuessed.pop();
+                break; // Exit loop if key is found
+            }
+        }
+    }
+
+    return keyExists;
 }
